af84397226b6465002e4a32d26bbe3d21b000063
Gean Sena <geansena990@gmail.com>
2023-03-05 23:14:12 -0300

    commit

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("ola Mundo, tudo bem? meu nome e gean marcos figueiredo sena moro em piritubae casa 192");
+    printf("ola Mundo, tudo bem? meu nome e gean marcos figueiredo sena moro em piritubae casa 192 lala la");
     
     return 0;
 }
