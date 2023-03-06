36c8f383ace8b2b534bea73f3d777896d2189cea
Gean Marcos Figueredo Sena <122699523+Gena26@users.noreply.github.com>
2023-03-05 23:06:11 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("ola Mundo, tudo bem? meu nome e gean marcos figueiredo sena");
-
+    printf("ola Mundo, tudo bem? meu nome e gean marcos figueiredo sena moro em pirituba");
+    
     return 0;
 }
